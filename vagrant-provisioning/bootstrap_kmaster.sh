9c37bd48b73e1d77d44a8e3191fbcde70ec91889
Rodriguez Ledezma <i.rodriguez.ledezma@accenture.com>
2019-03-04 14:39:30 -0600

    4G ram

--- vagrant-provisioning/bootstrap_kmaster.sh
+++ vagrant-provisioning/bootstrap_kmaster.sh
@@ -17,3 +17,25 @@ su - vagrant -c "kubectl create -f /vagrant/kube-flannel.yml"
 # Generate Cluster join command
 echo "[TASK 4] Generate and save cluster join command to /joincluster.sh"
 kubeadm token create --print-join-command > /joincluster.sh
+
+# Install Helm on k8s master
+echo "[TASK 5] install helm on master"
+curl https://raw.githubusercontent.com/helm/helm/master/scripts/get > get_helm.sh
+chmod 700 get_helm.sh
+./get_helm.sh
+helm init
+kubectl create serviceaccount --namespace kube-system tiller
+kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
+
+# Install git 
+echo "[TASK 6] install git on master"
+sudo yum install git -y
+
+# Install jenkins helm chart 
+echo "[TASK 7] install jenkins helm chart on master"
+mkdir charts
+cd charts
+git clone https://github.com/isbecks/helm-charts.git
+cd helm-charts/jenkins/
+helm install . --namespace=ops
